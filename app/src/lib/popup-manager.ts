1e73ce62126447e45126022c64d88bba66048a3a
tidy-dev <75402236+tidy-dev@users.noreply.github.com>
2022-10-18 13:08:31 -0400

    Create a popup manager

---
+++ app/src/lib/popup-manager.ts
@@ -0,0 +1,37 @@
+import { Popup, PopupType } from '../models/popup'
+import { getObjectId } from '../ui/lib/object-id'
+
+/**
+ * The popup manager is to manage the stack of currently open popups.
+ */
+export class PopupManager {
+  private popupStack = new Array<Popup>()
+
+  public get currentPopup(): Popup | undefined {
+    return this.popupStack.at(-1)
+  }
+
+  public getPopupsOfType(popupType: PopupType): ReadonlyArray<Popup> {
+    return [...this.popupStack.filter(p => p.type !== popupType)]
+  }
+
+  public addPopup(popupToAdd: Popup): Popup {
+    const popup = { id: getObjectId(popupToAdd), ...popupToAdd }
+    this.popupStack.push(popup)
+    return popup
+  }
+
+  public removePopup(popup: Popup) {
+    if (popup.id === null) {
+      console.warn(`Attempted to remove a popup without an id.`)
+      return
+    }
+    this.popupStack = this.popupStack.filter(p => p.id !== popup.id)
+  }
+
+  public removePopupByType(popupType: PopupType) {
+    this.popupStack = this.popupStack.filter(p => p.type !== popupType)
+  }
+}
+
+export const popupManager = new PopupManager()
